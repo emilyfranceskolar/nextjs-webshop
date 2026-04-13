f3fb2f0262f29cdf3baaac3cdf5f37aac0c27217
txmpold <love.tampold@hotmail.com>
2026-04-13 15:48:36 +0200

    create product card and page with placeholder content

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,59 @@
+import ProductCard from "@/components/ui/product-card";
+
 export default function ProductPage() {
   return (
     <main className="grid gap-4 place-items-center">
-      <p>Det här är produktsidan, där alla produkterna ska visas.</p>
+      <h1 className="text-2xl font-bold mt-10">Products</h1>
+      <section className="grid grid-cols-4 gap-4 p-4">
+        <ProductCard
+          title="Product 1"
+          description="More details about Product 1."
+          price={999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 2"
+          description="More details about Product 2."
+          price={1999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 3"
+          description="More details about Product 3."
+          price={2999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 4"
+          description="More details about Product 4."
+          price={3999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 5"
+          description="More details about Product 5."
+          price={4999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 6"
+          description="More details about Product 6."
+          price={5999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 7"
+          description="More details about Product 7."
+          price={6999}
+          imageUrl="/assets/images/test.avif"
+        />
+        <ProductCard
+          title="Product 8"
+          description="More details about Product 8."
+          price={7999}
+          imageUrl="/assets/images/test.avif"
+        />
+      </section>
     </main>
   );
 }
