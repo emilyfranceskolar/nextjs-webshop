a51ae682b6607454e7814177de73d754baceeb95
joscarlson90 <josefinecarlson90@gmail.com>
2026-04-14 15:27:39 +0200

    Co-authored-by: e_frances <emilyfranceskolar@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { db } from "@/prisma/db";
+import Link from "next/link";
+
+export default async function Home() {
+  const products = await db.product.findMany();
   return (
     <main className="grid gap-4 place-items-center">
       <p>Det här är startsidan. Här ska alla produkterna visas.</p>
+      <section>
+        {products.map((product) => (
+          <Link key={product.id} href={`/products/${product}`}>
+            <img
+              className="aspect-square object-center"
+              src={product.image}
+            ></img>
+          </Link>
+        ))}
+      </section>
     </main>
   );
 }
