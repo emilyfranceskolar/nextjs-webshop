9e0e25395fc4ea8ea212beceb9f0c9af2e53140d
joscarlson90 <josefinecarlson90@gmail.com>
2026-04-15 10:02:15 +0200

    add logo to header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-    <main className="grid gap-4 place-items-center">
+    <main className="grid gap-4 p-6 place-items-center">
       <p>Det här är startsidan. Här ska alla produkterna visas.</p>
+      <p className="p-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dignissimos
+        voluptatem neque culpa delectus ipsum quis ipsam vel dicta dolorem! Ex
+        alias enim vitae dolor hic accusantium placeat, beatae esse.<br></br> Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Quidem excepturi placeat
+        omnis, incidunt natus at quaerat, magnam repellat assumenda cum ducimus
+        molestias beatae qui velit, nobis cumque vero rerum voluptatibus! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit.<br></br> Aspernatur sint nulla
+        odio odit, atque consequuntur in molestias repudiandae aliquid
+        architecto? Facere vel saepe dolorum nobis, quidem amet suscipit autem
+        eligendi. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Fugiat possimus quasi excepturi earum. Sint nisi quae eius enim optio!
+        Ea rem error at omnis! Aut rerum doloribus officia placeat magnam.
+      </p>
     </main>
   );
 }
