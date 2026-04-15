9e774f281cffda7a8b3b24bbca34bca2a3b7af8c
txmpold <love.tampold@hotmail.com>
2026-04-15 12:51:29 +0200

    update product page for database use

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,58 +1,22 @@
 import ProductCard from "@/components/ui/product-page-card";
+import { db } from "@/prisma/db";
 
 export default function ProductPage() {
+  const products = db.product.findMany({});
+
   return (
     <main className="grid gap-4 place-items-center">
       <h1 className="text-2xl font-bold mt-10">Products</h1>
       <section className="grid grid-cols-4 gap-4 p-4">
-        <ProductCard
-          title="Product 1"
-          description="More details about Product 1."
-          price={999}
-          imageUrl="/assets/images/shirt-black.jpg"
-        />
-        <ProductCard
-          title="Product 2"
-          description="More details about Product 2."
-          price={1999}
-          imageUrl="/assets/images/shirt-red.jpg"
-        />
-        <ProductCard
-          title="Product 3"
-          description="More details about Product 3."
-          price={2999}
-          imageUrl="/assets/images/shirt-white-blue.jpg"
-        />
-        <ProductCard
-          title="Product 4"
-          description="More details about Product 4."
-          price={3999}
-          imageUrl="/assets/images/shirt-grey.jpg"
-        />
-        <ProductCard
-          title="Product 5"
-          description="More details about Product 5."
-          price={4999}
-          imageUrl="/assets/images/shirt-white.jpg"
-        />
-        <ProductCard
-          title="Product 6"
-          description="More details about Product 6."
-          price={5999}
-          imageUrl="/assets/images/test.avif"
-        />
-        <ProductCard
-          title="Product 7"
-          description="More details about Product 7."
-          price={6999}
-          imageUrl="/assets/images/test.avif"
-        />
-        <ProductCard
-          title="Product 8"
-          description="More details about Product 8."
-          price={7999}
-          imageUrl="/assets/images/test.avif"
-        />
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            description={product.description}
+            price={product.price}
+            imageUrl={product.image}
+          />
+        ))}
       </section>
     </main>
   );
