39fc79daf5ebe36ca37f234a7a2ce6ea832d1d97
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-04-15 13:48:22 +0200

    add first text to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,10 @@
 export default function CartPage() {
   return (
-    <main className="grid gap-4 place-items-center">
-      <p>Det här är kassasidan.</p>
+    <main className="grid place-item-center">
+      <section className="grid gap-16 p-12">
+        <h1 className="text-3xl">Some text here?</h1>
+        <h3 className="text-3xl">Your shopping cart is currently empty</h3>
+      </section>
     </main>
   );
 }
