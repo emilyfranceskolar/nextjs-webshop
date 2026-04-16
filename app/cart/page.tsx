0bf0311c7325fd53c0950812b31be0be6c48a1ca
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-04-16 14:18:06 +0200

    first iteration of cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,8 +4,6 @@ export default function CartPage() {
   return (
     <main>
       <section className="grid place-items-center gap-16 p-12">
-        <h1 className="flex gap-2 text-3xl font-bold">Shopping cart</h1>
-        <p>Your cart is currently empty.</p>
         <ShoppingCartList />
       </section>
     </main>
