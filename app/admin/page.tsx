be90adf392cb4a2bdceab42757f57abaa3f972c7
joscarlson90 <josefinecarlson90@gmail.com>
2026-04-16 12:07:27 +0200

    add btn-components to admin-page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,5 @@
-import { HomeButton } from "@/components/home-button";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
 import { db } from "@/prisma/db";
 
 
@@ -7,19 +8,26 @@ export default async function AdminPage() {
   return (
     <main className="grid gap-4 place-items-center">
       <p className="text-3xl font-bold m-10">Our products</p>
-      <section className="grid w-full gap-6 p-4 sm:grid-cols-2 xl:grid-cols-4">{products.map((product) => (
+      <section className="grid w-full gap-6 p-4 sm:grid-cols-2 xl:grid-cols-3">{products.map((product) => (
         <article key={product.id} data-cy="product-id" className="flex justify-center gap-6 mb-4">
           <img
             className="object-cover rounded-lg w-24 h-auto"
             src={product.image}
             alt={product.title}
           />
-          <div className="grid items-center">
-            <p data-cy="product-title" className="flex justify-center font-bold text-sm">{product.title}</p>
-            <p data-cy="product-price" className="flex justify-center text-sm">{product.price}kr</p>
+          <div className="grid items-start">
+            <section className="p-2">
+              <p data-cy="product-title" className="font-bold text-sm">{product.title}</p>
+              <p data-cy="product-price" className="text-sm">{product.price}kr</p>
+            </section>
+
+            <ButtonGroup>
+              <Button variant="outline">Edit product</Button>
+              <Button variant="outline" className="hover:bg-red-200">Delete product</Button>
+            </ButtonGroup>
             {/* <Link href={""} data-cy="admin-edit-product"><HomeButton className="hover:bg-green-100 w-full">Add product</HomeButton></Link> */}
-            <HomeButton className=" hover:bg-yellow-100 text-xs">Edit product</HomeButton>
-            <HomeButton className=" hover:bg-red-200 text-xs">Delete product</HomeButton>
+            {/* <HomeButton className=" hover:bg-yellow-100 text-xs">Edit product</HomeButton>
+            <HomeButton className=" hover:bg-red-200 text-xs">Delete product</HomeButton> */}
           </div>
         </article>
 
