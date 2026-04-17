d27f696c8ef7846d1b2511da55a59045f07bd1fc
joscarlson90 <josefinecarlson90@gmail.com>
2026-04-17 15:06:32 +0200

    change empty-card (again), deletebtn works

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -20,19 +20,20 @@ export default async function AdminPage() {
   return (
     <main className="grid">
       <p className="text-3xl font-bold m-10 text-center">Our products</p>
-      <section className="grid gap-4 px-4 sm:grid-cols-2 xl:grid-cols-3">
+      <section className="grid gap-4 items-stretch px-4 sm:grid-cols-2 xl:grid-cols-3">
         <Link href="/admin/new">
-          <div className="flex gap-6 items-center p-4 border rounded-xl w-full hover:bg-muted/50 transition">
-            <div className="w-24 h-28 rounded-xl border-2 border-dashed flex items-center justify-center text-sm text-muted-foreground">
+          <div className="flex gap-6 items-center p-4 border rounded-xl w-full hover:bg-muted/50 transition h-full">
+            <div className="w-24 h-28 rounded-lg border-2 border-dashed flex items-center justify-center text-sm text-muted-foreground">
               Image
             </div>
 
-            <div className="flex flex-col gap-2">
-              <div>
+            <div className="flex flex-wrap gap-2 px-2 py-4 rounded-xl h-full">
+              <div className="gap-2 px-2 py-4">
                 <p className="font-semibold text-lg text-zinc-700">
                   New product
                 </p>
                 <p className="text-zinc-600">0 kr</p>
+                <p className="text-zinc-600 text-sm">No description</p>
               </div>
               <Button variant="outline">
                 <Plus className="mr-2 h-4 w-4" />
@@ -43,7 +44,7 @@ export default async function AdminPage() {
         </Link>
 
         {products.map((product) => (
-          <article key={product.id} data-cy="product" className="flex flex-wrap gap-2 px-2 py-4 border rounded-xl">
+          <article key={product.id} data-cy="product" className="flex flex-wrap gap-2 px-2 py-2 border h-full rounded-xl">
             <img
               className="object-cover rounded-lg w-24 h-28"
               src={product.image}
