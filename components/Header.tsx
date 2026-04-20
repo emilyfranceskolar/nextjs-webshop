b4de661bb14787270dca8373a197c491f5ab1af4
txmpold <love.tampold@hotmail.com>
2026-04-20 13:13:01 +0200

    add data cy to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,37 @@
 "use client";
-import { useCart } from "@/hooks/use-cart";
+import { useCartContext } from "@/app/providers/cart-provider";
 import Link from "next/link";
 
 export default function Header() {
-  const { productsInCart } = useCart();
+  const { productsInCart, isLoaded } = useCartContext();
 
   return (
     <header className="flex items-center justify-between border-b-26 border-rose-900 px-8 py-10">
       <Link
         href="/"
-        className="bg-[url('/assets/images/worn-stories-logo.png')] bg-contain bg-no-repeat bg-c w-55 h-30 text-4xl text-black"
+        className="bg-[url('/assets/images/worn-stories-logo.png')] bg-contain bg-no-repeat bg-center w-55 h-30 text-4xl text-black"
       ></Link>
       <nav className="flex gap-8">
         <Link
           href="/product"
           className="text-zinc-600 hover:underline underline-offset-8 decoration-2 decoration-zinc-500"
         >
-          Products
+          <p>Products</p>
         </Link>
         <Link
           href="/admin"
           className="text-zinc-600 hover:underline underline-offset-8 decoration-2 decoration-zinc-500"
         >
-          Admin
+          <p>Admin</p>
         </Link>
         <Link
           href="/cart"
           className="text-zinc-600 hover:underline underline-offset-8 decoration-2 decoration-zinc-500"
+          data-cy="cart-link"
         >
-          Cart ({productsInCart.length})
+          <p data-cy="cart-items-count-badge">
+            Cart ({isLoaded ? productsInCart.length : 0})
+          </p>
         </Link>
       </nav>
     </header>
