c20faf85a52e092003a9a287f7c86b2cf6385aac
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-04-20 13:29:02 +0200

    Add select country component

---
+++ components/select-country.tsx
@@ -0,0 +1,27 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export function SelectCountry() {
+  return (
+    <Select>
+      <SelectTrigger className="w-full max-w-lg">
+        <SelectValue placeholder="Country/Region" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="apple">Sweden</SelectItem>
+          <SelectItem value="banana">Norway</SelectItem>
+          <SelectItem value="blueberry">Denmark</SelectItem>
+          <SelectItem value="grapes">Finland</SelectItem>
+          <SelectItem value="pineapple">Germany</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
