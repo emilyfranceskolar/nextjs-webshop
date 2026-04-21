c376ccbd4224b23e0fe9e423d35bbd86835065a8
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-04-21 14:01:34 +0200

    create customerSchema for zod validation

--- data/form.tsx
+++ data/form.tsx
@@ -1,27 +1,14 @@
 import { z } from "zod";
 
-export const formSchema = z.object({
-  email: z.email(),
-  firstName: z
-    .string()
-    .min(2, { error: "First name must be at least 2 characters." })
-    .max(14, { error: "First name must be at most 14 characters." }),
-  lastName: z
-    .string()
-    .min(2, { error: "Last name must be at least 2 characters." })
-    .max(14, { error: "Last name must be at most 14 characters." }),
-  address: z
-    .string()
-    .min(5, { error: "Address must be at least 5 characters." })
-    .max(14, { error: "Address must be at most 14 characters." }),
-  address2: z
-    .string()
-    .min(2, { error: "Address must be at least 5 characters." })
-    .max(14, { error: "Address must be at most 14 characters." }),
-  city: z.string().min(5).max(18),
-  postalCode: z.coerce.number().min(5).max(8),
-
-  phoneNr: z.coerce.number().min(10).max(13),
+export const customerSchema = z.object({
+  email: z.email({ error: "Enter an email" }),
+  name: z.string().min(1, { error: "Enter a first name" }),
+  address: z.string().min(1, { error: "Enter an address" }),
+  postalCode: z.coerce
+    .number<number>()
+    .min(1, { error: "Enter a ZIP/ postal code" }),
+  city: z.string().min(1, { error: "Enter a city" }),
+  phoneNr: z.string().min(1, { error: "Enter a phone number" }),
 });
 
-export type Payment = z.infer<typeof formSchema>;
+export type Customer = z.infer<typeof customerSchema>;
