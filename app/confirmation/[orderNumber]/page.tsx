fb413c56ff2f01612cac64c15f28af538dbefeb3
joscarlson90 <josefinecarlson90@gmail.com>
2026-04-21 10:59:16 +0200

    add Link import to confirmation-page

--- app/confirmation/[orderNumber]/page.tsx
+++ app/confirmation/[orderNumber]/page.tsx
@@ -1,10 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function ConfirmationPage({
   params,
 }: {
-  params: { orderNumber: string }
+  params: { orderNumber: string, amount: string, orderDate: Date, total: string, image: string, title: string, price: string };
 }) {
 
   return (
@@ -12,7 +13,7 @@ export default function ConfirmationPage({
       <div className="max-w-xl mx-auto w-full rounded-xl p-4 m-6">
         <Card className="min-h-[60vh] flex justify-center pl-8 pr-8">
           <CardHeader>
-            <CardTitle className="text-2xl">
+            <CardTitle className="text-2xl pb-2">
               Payment succeeded! 🎉
             </CardTitle>
           </CardHeader>
@@ -22,20 +23,23 @@ export default function ConfirmationPage({
               Thank you for your purchase
             </p>
 
-            <div className="bg-muted p-4 rounded-xl">
+            <div data-cy="product" className="bg-muted p-4 rounded-xl">
               <div>
                 <p className="pb-2"><strong>Order Number:</strong> #{params.orderNumber}</p>
-                <p className="pb-2"><strong>Amount:</strong> 3</p>
                 <p className="pb-2"><strong>Order Date:</strong> 2026-01-14</p>
-                <p className="pb-2"><strong>Total:</strong> 899 kr</p>
+                {/* <img src={params.image} alt="Product Image">Product Image</img> */}
+                <p data-cy="product-title" className="pb-2"><strong>Title:</strong> {params.title}</p>
+                <p className="pb-2"><strong>Amount:</strong> {params.amount}</p>
+                <p data-cy="product-price" className="pb-2"><strong>Price:</strong> {params.price}</p>
+                <p className="pb-2"><strong>Total:</strong> {params.total}</p>
               </div>
             </div>
 
-            <div>
+            <Link href="/product">
               <Button className="p-4 w-full hover:bg-zinc-800 mt-auto">
                 Continue shopping
               </Button>
-            </div>
+            </Link>
           </CardContent>
         </Card>
       </div>
