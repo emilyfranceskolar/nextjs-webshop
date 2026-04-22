b0ab4d2657cc66c861648e1819ef19e4644b01f4
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-04-22 13:54:13 +0200

    remove used code

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,10 +3,6 @@ import ShoppingCartList from "@/components/shopping-cart";
 import { Separator } from "@/components/ui/separator";
 
 export default function DeliveryPage() {
-  const handlePayment = async () => {
-    const orderNumber = Math.floor(100000 + Math.random() * 900000);
-  };
-
   return (
     <div className="flex justify-between px-25 py-10 gap-10 md:flex-row flex-col">
       <Form />
