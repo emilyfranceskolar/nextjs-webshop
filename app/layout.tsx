21e48f8e02b398a9fec3a9b9a0a5f94dd2ebc848
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-04-23 08:17:38 +0200

    change to lowercase for all components, update the import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/footer";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import { cn } from "@/lib/utils";
 import { Geist, Inter } from "next/font/google";
 import type { Metadata } from "next/types";
