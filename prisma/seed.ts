1498108b7865bca32db56b5ef987074691514f37
joscarlson90 <josefinecarlson90@gmail.com>
2026-09-03 16:41:34 +0200

    trying to fix bug?

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,11 +2,39 @@ import { products } from "@/data";
 import { db } from "./db";
 
 async function main() {
-  for (const { id, ...product } of products) {
-    await db.product.upsert({
-      where: { articleNumber: product.articleNumber },
+  for (const { id, category, ...product } of products) {
+    const categoryRecord = await db.category.upsert({
+      where: {
+        name: category,
+      },
       update: {},
-      create: product,
+      create: {
+        name: category,
+        slug: category.toLowerCase(),
+      },
+    });
+
+    await db.product.upsert({
+      where: {
+        articleNumber: product.articleNumber,
+      },
+      update: {
+        ...product,
+        categories: {
+          deleteMany: {},
+          create: {
+            categoryId: categoryRecord.id,
+          },
+        },
+      },
+      create: {
+        ...product,
+        categories: {
+          create: {
+            categoryId: categoryRecord.id,
+          },
+        },
+      },
     });
   }
 }
